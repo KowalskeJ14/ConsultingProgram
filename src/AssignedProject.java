f6fc9f4bf8c309e324c062dec6b2b8b01b7c62e2
kowal_000 <kowal_000@JaredsAsus.attlocal.net>
2015-03-08 00:32:40 -0600

    Built the framework for the program. Will add changes as needed. No methods include code yet.

---
+++ src/AssignedProject.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kowal_000
+ */
+public class AssignedProject {
+    private Invoice invoice = new Invoice();
+
+    public AssignedProject() {
+    }
+
+    public Invoice getInvoice() {
+        return invoice;
+    }
+
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
+    }
+    
+    public void startProject() {
+        
+    }
+    
+    public void endProject() {
+        
+    }
+    
+    public void addExpense() {
+        
+    }
+    
+    public void addBillableActivity() {
+        
+    }
+}
